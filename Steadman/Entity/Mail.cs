6e0ef59b95389a2e009259847736718d99fd611a
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-03-09 22:51:29 +0300

    finished logic part

--- Steadman/Entity/Mail.cs
+++ Steadman/Entity/Mail.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,13 @@ namespace Steadman.Entity
     public class Mail
     {
         public int Id { get; set; }
+
+        [Display(Name = "Email address")]
+        [Required(ErrorMessage = "The email address is required")]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string MailAddress { get; set; }
+
+        public DateTime CreateTime { get; set; } = DateTime.Now;
     }
 }
--- Steadman/Pages/Book.cshtml.cs
+++ Steadman/Pages/Book.cshtml.cs
@@ -4,20 +4,32 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Steadman.Entity;
 
 namespace Steadman.Pages
 {
     public class BookModel : PageModel
     {
-        public string Message { get; set; }
+        [BindProperty]
+        public Mail Mail { get; set; } = new Mail();
+
+        public string Message { get; set; } = string.Empty;
 
         public void OnGet()
         {
-
         }
 
         public IActionResult OnPost()
         {
+            if(ModelState.IsValid)
+            {
+                var asd = Mail.CreateTime;
+                var ss = Mail.MailAddress;
+                var ee = Mail.Id;
+
+                Message = "We thank you for your interest. We will finish the book in a very short time. We will send it to you when we are finished.";
+            }
+
 
             return Page();
         }
