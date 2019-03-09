1825f32708f8ac07a5c867572e0a503401840dfd
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-03-09 21:07:39 +0300

    download screen added

---
+++ Steadman/Pages/Book.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Steadman.Pages
+{
+    public class BookModel : PageModel
+    {
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
