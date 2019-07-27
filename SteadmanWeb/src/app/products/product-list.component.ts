1f8f8a593b01d31e2326abfe2a1938ad2cb066e5
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-07-27 16:41:30 +0300

    toggleImage

--- SteadmanWeb/src/app/products/product-list.component.ts
+++ SteadmanWeb/src/app/products/product-list.component.ts
@@ -8,6 +8,7 @@ export class ProductListComponent {
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
+    showImage: boolean = false;
     products: any[] = [
         {
             "productId": 1,
@@ -30,4 +31,8 @@ export class ProductListComponent {
             "imageUrl": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
           }
     ];
+
+    toggleImage(): void {
+      this.showImage = !this.showImage;
+    }
 }
