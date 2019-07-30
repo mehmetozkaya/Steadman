1b0e37799bc90193f6c9f9e73cf606bd43176e7a
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-07-30 17:35:03 +0300

    style added

--- SteadmanWeb/src/app/products/product-list.component.ts
+++ SteadmanWeb/src/app/products/product-list.component.ts
@@ -1,8 +1,10 @@
 import { Component } from "@angular/core";
+import { IProduct } from "./product";
 
 @Component({
     selector: 'pm-products',
-    templateUrl: './product-list.component.html'
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
     pageTitle: string = 'Product List';
@@ -10,7 +12,7 @@ export class ProductListComponent {
     imageMargin: number = 2;
     showImage: boolean = false;
     listFilter: string = 'cart';
-    products: any[] = [
+    products: IProduct[] = [
         {
             "productId": 1,
             "productName": "Leaf Rake",
