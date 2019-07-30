bc0eb62e5e1afbd0315b2be578f9588338b8891f
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-07-30 17:40:13 +0300

    ngOnInit

--- SteadmanWeb/src/app/products/product-list.component.ts
+++ SteadmanWeb/src/app/products/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./product";
 
 @Component({
@@ -6,7 +6,8 @@ import { IProduct } from "./product";
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+ 
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
@@ -38,4 +39,8 @@ export class ProductListComponent {
     toggleImage(): void {
       this.showImage = !this.showImage;
     }  
+
+    ngOnInit(): void {
+      console.log('mozkaya');
+    }
 }
