0b9f905ac8ab5261aee450ec8f8afc8701998718
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-07-30 18:44:33 +0300

    performFilter

--- SteadmanWeb/src/app/products/product-list.component.ts
+++ SteadmanWeb/src/app/products/product-list.component.ts
@@ -7,12 +7,22 @@ import { IProduct } from "./product";
     styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
- 
+   
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
-    listFilter: string = 'cart';
+
+    _listFilter: string = 'cart';
+    get listFilter(): string {
+      return this._listFilter;
+    }
+    set listFilter(value: string) {
+      this._listFilter = value;
+      this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+    }
+
+    filteredProducts: IProduct[];
     products: IProduct[] = [
         {
             "productId": 1,
@@ -43,4 +53,11 @@ export class ProductListComponent implements OnInit {
     ngOnInit(): void {
       console.log('mozkaya');
     }
+
+    performFilter(filterBy: string): IProduct[] {
+      filterBy = filterBy.toLocaleLowerCase();
+      return this.products.filter((product: IProduct) => 
+              product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+    }
+
 }
