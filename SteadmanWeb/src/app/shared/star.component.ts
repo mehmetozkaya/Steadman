78313f5971ccceac714331bc129f06d9fb7041ee
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-07-30 20:44:51 +0300

    star

--- SteadmanWeb/src/app/shared/star.component.ts
+++ SteadmanWeb/src/app/shared/star.component.ts
@@ -1,10 +1,17 @@
-import { Component } from "@angular/core";
+import { Component, OnChanges } from "@angular/core";
 
 @Component({
     selector: 'pm-star',
     templateUrl: './star.component.html',
     styleUrls: ['./star.component.css']
 })
-export class StarComponent {
+export class StarComponent implements OnChanges {
     
+    rating: number = 4;
+    starWidth: number;
+
+    ngOnChanges(): void {
+        this.starWidth = this.rating * 75 / 5;
+    }
+
 }
