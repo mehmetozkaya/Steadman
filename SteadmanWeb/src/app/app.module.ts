8bb191be2adca12606df54269090c1b6b39599ac
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-08-25 13:26:11 +0300

    product-detail added

--- SteadmanWeb/src/app/app.module.ts
+++ SteadmanWeb/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ProductDetailComponent } from './products/product-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ConvertToSpacesPipe,
-    StarComponent
+    StarComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
