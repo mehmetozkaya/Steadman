bdbd577d488425f149a722d7d7c6a035aeffb55e
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-08-25 13:30:35 +0300

    pageTitle and product properties added

--- SteadmanWeb/src/app/products/product-detail.component.ts
+++ SteadmanWeb/src/app/products/product-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product';
 
 @Component({
   selector: 'pm-product-detail',
@@ -7,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailComponent implements OnInit {
 
+  pageTitle: string = 'Product Detail';
+  product: IProduct;
+  
   constructor() { }
 
   ngOnInit() {
