54bf2bb70c3f87e569a511e45aee62b78ccd8ae9
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-08-26 12:53:19 +0300

    refactor

--- SteadmanWeb/src/app/products/product.module.ts
+++ SteadmanWeb/src/app/products/product.module.ts
@@ -4,16 +4,20 @@ import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
 import { StarComponent } from '../shared/star.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule
+  ],
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
     ConvertToSpacesPipe,
     StarComponent
-  ],
-  imports: [
-    CommonModule
   ]
 })
 export class ProductModule { }
